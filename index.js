6049d6d7551dbc5d66cb065f894311fb6c5f265f
Danilo Gondim <daniloencarnacao@gmail.com>
2020-08-28 22:11:12 +0000

    Linted the directory files

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 // Create a new task object
 // A new task will be created as incomplete
-function newTask(title, description) {
+const newTask = (title, description) => {
   const task = {
     title,
     description,
     complete: false,
 
-    logState: function () {
+    logState: function() {
       console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
     },
 
-    markCompleted: function () {
+    markCompleted: function() {
       this.complete = true;
     }
 
   };
   return task;
-}
+};
 
 // DRIVER CODE BELOW
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
